0a15aafda2ffcda7cf6864c17dcb8eb7bf2ff804
Mouadhbendjedidi <alfadjr2007@gmail.com>
2025-06-27 10:22:10 +0100

    getting rude of some packages

--- modules/home/default.nix
+++ modules/home/default.nix
@@ -1,7 +1,6 @@
 { ... }:
 {
   imports = [
-   #./aseprite/aseprite.nix           # pixel art editor
     ./audacious.nix                   # music player
     ./bat.nix                         # better cat command
     ./browser.nix                     # firefox based browser
@@ -9,36 +8,27 @@
     ./cava.nix                        # audio visualizer
     ./discord.nix                     # discord
     ./fastfetch.nix                   # fetch tool
-   #./flow.nix                        # terminal text editor
-   #./fzf.nix                         # fuzzy finder
-   #./gaming.nix                      # packages related to gaming
-   #./ghostty.nix                     # terminal
+    ./ghostty.nix                     # terminal
     ./git.nix                         # version control
     ./gnome.nix                       # gnome apps
     ./gtk.nix                         # gtk theme
     ./hyprland                        # window manager
     ./kitty.nix                       # terminal
     ./lazygit.nix
-   # ./micro.nix                       # nano replacement
-   # ./nemo.nix                        # file manager
-   # ./nix-search/nix-search.nix       # TUI to search nixpkgs
+    ./micro.nix                       # nano replacement
+    ./nemo.nix                        # file manager
+    ./nix-search/nix-search.nix       # TUI to search nixpkgs
     ./nvim.nix                        # neovim editor
     ./obsidian.nix
-   # ./p10k/p10k.nix
     ./packages                        # other packages
-   # ./retroarch.nix  
     ./rofi.nix                        # launcher
-    # ./scripts/scripts.nix             # personal scripts
-   # ./ssh.nix                         # ssh config
-    ./superfile/superfile.nix         # terminal file manager
+    ./ssh.nix                         # ssh config
     ./swaylock.nix                    # lock screen
     ./swayosd.nix                     # brightness / volume wiget
     ./swaync/swaync.nix               # notification deamon
-    # ./viewnior.nix                    # image viewer
-    # ./vscodium                        # vscode fork
+    ./vscodium                        # vscode fork
     ./waybar                          # status bar
     ./waypaper.nix                    # GUI wallpaper picker
-    ./xdg-mimes.nix                   # xdg config
     ./zsh                             # shell
   ];
 }
