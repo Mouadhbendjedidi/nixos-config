276628afde9e90c05c319288e6441a8231f94504
Mouadh Bendjedidi <111908865+Mouadhbendjedidi@users.noreply.github.com>
2025-06-28 10:42:04 +0100

    fix git

--- modules/home/git.nix
+++ modules/home/git.nix
@@ -7,12 +7,34 @@
     userEmail = "alfadjr2007@gmail.com";
 
     extraConfig = {
-      core.editor = "nvim";
       init.defaultBranch = "main";
+      merge.conflictstyle = "diff3";
+      diff.colorMoved = "default";
+      pull.ff = "only";
       color.ui = true;
+      url = {
+        "git@github.com:".insteadOf = [
+          "gh:"
+          "https://github.com/"
+        ];
+        "git@github.com:nuexq/".insteadOf = "fp:";
+      };
+      core.excludesFile = "/home/${username}/.config/git/.gitignore";
     };
 
+    delta = {
+      enable = true;
+      options = {
+        line-numbers = true;
+        side-by-side = false;
+        diff-so-fancy = true;
+        navigate = true;
+      };
+    };
+  };
+
   home.packages = [ pkgs.gh ]; # pkgs.git-lfs
+
 
   programs.zsh.shellAliases = {
     g = "lazygit";
