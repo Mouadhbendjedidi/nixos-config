7e1f4aa0396fcc6247bef6dad7c1fe2a24586c57
Mouadhbendjedidi <alfadjr2007@gmail.com>
2026-04-14 23:49:14 +0100

    fix signature

--- home/mouadh/home/git.nix
+++ home/mouadh/home/git.nix
@@ -12,7 +12,6 @@
   programs.git = {
     enable = true;
     signing = {
-      key = "~/.ssh/id_ed25519.pub";
       signByDefault = true;
     };
     settings = {
@@ -21,6 +20,8 @@
       commit.gpgsign = true;
       gpg.format = "ssh";
       gpg.ssh.allowedSignersFile = "~/.config/git/allowed_signers";
+      user.signingKey = "~/.ssh/id_ed25519.pub";
+      gpg.ssh.signingKey = "~/.ssh/id_ed25519.pub";
       core.editor = "nvim";
       init.defaultBranch = "main";
       pull.rebase = true;
@@ -39,6 +40,6 @@
   };
 
   home.file.".config/git/allowed_signers".text = ''
-    alfadjr2007@gmail.com sk-ssh-ed25519 YOUR_PUBLIC_KEY_CONTENT
+    alfadjr2007@gmail.com ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIO5mu2tvT1ERq+h7ukJmbC1IyOas+oROdwHqJfr1veRm
   '';
 }
