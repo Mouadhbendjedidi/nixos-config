9eb7b9ebc6444a04104621cd2b9b50774e88b52f
Mouadhbendjedidi <alfadjr2007@gmail.com>
2026-04-15 23:10:21 +0100

    yellow

--- home/mouadh/home/dooit.nix
+++ home/mouadh/home/dooit.nix
@@ -76,7 +76,7 @@
         fmt.todos.effort.add(effort_icon(icon="󱠇 "))
 
         # --------- DESCRIPTION ---------
-        format = Text("  {completed_count}/{total_count}", style=theme.green).markup
+        format = Text("  {completed_count}/{total_count}", style=theme.yellow).markup
         fmt.todos.description.add(todo_description_progress(fmt=format))
         fmt.todos.description.add(description_highlight_tags(fmt="󰌪 {}"))
         fmt.todos.description.add(description_strike_completed())
